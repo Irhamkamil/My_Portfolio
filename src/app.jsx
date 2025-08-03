520a2004b7a0c3505d7d535501ac38f262565475
irhamkamil <irhamkamil8322@gmail.com>
2025-08-03 21:06:18 +0700

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const app = () => {
+  return (
+    <main>
+      <div>
+        <h1 className="text-3xl underline">Welcome To My Portfolio!</h1>
+      </div>
+    </main>
+  );
+};
+
+export default app;
